ce564724991ca033ea531e9f5db3d4dc8823577e
zhonghuatu <1787691205@qq.com>
2019-08-05 16:01:23 +0800

    hehe

--- jd4/daemon.py
+++ jd4/daemon.py
@@ -42,9 +42,13 @@ class JudgeHandler:
         self.rid = self.request.pop('rid')
         self.lang = self.request.pop('lang')
         self.code = self.request.pop('code')
+        self.remote = self.request.pop('remote')
         try:
             if self.type == 0:
-                await self.do_submission()
+				if self.remote==False:
+					await self.do_submission()
+				else:
+					await self.do_submission_remote()
             elif self.type == 1:
                 await self.do_pretest()
             else:
@@ -72,6 +76,14 @@ class JudgeHandler:
         package = await self.build()
         with await cases_file_task as cases_file:
             await self.judge(cases_file, package)
+
+    async def do_submission_remote(self):
+        loop = get_event_loop()
+        logger.info('Submission Remote: %s, %s, %s ( %s, %s )', self.domain_id, self.pid, self.rid,self.remote['orig_oj'],self.remote['orig_id'])
+        #cases_file_task = loop.create_task(cache_open(self.session, self.domain_id, self.pid))
+        #package = await self.build()
+        with await cases_file_task as cases_file:
+            await self.judge_remote()
 
     async def do_pretest(self):
         loop = get_event_loop()
@@ -122,6 +134,40 @@ class JudgeHandler:
                  score=total_score,
                  time_ms=total_time_usage_ns // 1000000,
                  memory_kb=total_memory_usage_bytes // 1024)
+				 
+	async def judge(self, cases_file, package):
+        loop = get_event_loop()
+        self.next(status=STATUS_JUDGING, progress=0)
+        #cases = list(read_cases(cases_file))
+        total_status = STATUS_ACCEPTED
+        total_score = 100
+        total_time_usage_ns = 0
+        total_memory_usage_bytes = 0
+        '''judge_tasks = list()
+        for case in cases:
+            judge_tasks.append(loop.create_task(case.judge(package)))
+        for index, judge_task in enumerate(judge_tasks):
+            status, score, time_usage_ns, memory_usage_bytes, stderr = await shield(judge_task)
+            if self.type == 1:
+                judge_text = stderr.decode(encoding='utf-8', errors='replace')
+            else:
+                judge_text = ''
+            self.next(status=STATUS_JUDGING,
+                      case={'status': status,
+                            'score': score,
+                            'time_ms': time_usage_ns // 1000000,
+                            'memory_kb': memory_usage_bytes // 1024,
+                            'judge_text': judge_text},
+                      progress=(index + 1) * 100 // len(cases))
+            total_status = max(total_status, status)
+            total_score += score
+            total_time_usage_ns += time_usage_ns
+            total_memory_usage_bytes = max(total_memory_usage_bytes, memory_usage_bytes)'''
+        self.end(status=total_status,
+                 score=total_score,
+                 time_ms=total_time_usage_ns // 1000000,
+                 memory_kb=total_memory_usage_bytes // 1024,
+				 judge_text="Test")
 
     def next(self, **kwargs):
         self.ws.send_json({'key': 'next', 'tag': self.tag, **kwargs})
